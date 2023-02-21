e898f12a7a9b273a14f653e84435f9661b5dd410
hochstegerL03 <72703264+hochstegerL03@users.noreply.github.com>
2023-02-21 09:22:08 +0100

    V.0.0

---
+++ rate-me/server/app.js
@@ -0,0 +1,10 @@
+import jsonServer from 'json-server';
+const server = jsonServer.create();
+const router = jsonServer.router('employees.json');
+const middlewares = jsonServer.defaults();
+const port = process.env.PORT || 3000;
+
+server.use(middlewares);
+server.use(router);
+
+server.listen(port, () => console.log('server is running ...'));
