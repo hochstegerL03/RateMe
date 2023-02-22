3a6c1411124d21c4c244b7c275e7cff67c818a78
hochstegerL03 <72703264+hochstegerL03@users.noreply.github.com>
2023-02-22 11:10:37 +0100

    V.0.1

--- rate-me/vite.config.js
+++ rate-me/vite.config.js
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import { quasar, transformAssetUrls } from '@quasar/vite-plugin';
+import { VitePWA } from 'vite-plugin-pwa';
+import manifest from './manifest.js';
 
 export default defineConfig({
   plugins: [
@@ -10,6 +12,13 @@ export default defineConfig({
 
     quasar({
       sassVariables: 'src/quasar-variables.sass',
+    }),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest,
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico}'],
+      },
     }),
   ],
 });
